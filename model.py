4d7f8b299ff492c138961234b2209e7c6da26f0a
Georg Hess <georg.hess@zenseact.com>
2022-05-17 10:38:22 +0200

    load sst

--- model.py
+++ model.py
@@ -1,11 +1,23 @@
 from torch import nn
+from mmcv import Config
+from mmdet3d.models import build_model
 
+
+def build_sst(config_path):
+    config_path = "SST/configs/sst_refactor/sst_encoder_only.py"
+    cfg = Config.fromfile(config_path)
+    model = build_model(
+        cfg.model,
+        train_cfg=cfg.get('train_cfg'),
+        test_cfg=cfg.get('test_cfg'))
+    model.init_weights()
+    return model
 
 class LidarEncoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, sst_config_path):
         super().__init__()
-        self._sst = build_sst()
+        self._sst = build_sst(sst_config_path)
         self._pooler = build_pooler()
 
     def forward(self, point_cloud):
