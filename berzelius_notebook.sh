b2ffb7565f862c9502de8a67b85e1327ee66dd48
Adam Tonderski <adam.tonderski@zenseact.com>
2022-06-02 17:19:16 +0200

    add notebook

---
+++ berzelius_notebook.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --nodes 1
+#SBATCH --gpus 1
+#SBATCH --time 3-00:00:00
+#SBATCH --output /proj/nlp4adas/users/%u/logs/%j.out
+#SBATCH -A berzelius-2022-117
+#
+
+singularity exec --nv --bind /proj/nlp4adas/users/$USER:/workspace \
+  --bind /proj/nlp4adas/datasets/once:/my_data \
+  --pwd /workspace/lidar-clippin/ \
+  --env PYTHONPATH=/workspace/lidar-clippin/ \
+  /proj/nlp4adas/containers/lidar-clippin.sif \
+  jupyter notebook --notebook-dir=/workspace --ip 0.0.0.0 --no-browser --allow-root
+
+#
+#EOF
