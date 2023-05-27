fbda8b9a8450805457c080d1e85f7ce38cf25f3a
Adam Tonderski <adam.tonderski@zenseact.com>
2023-05-27 18:56:01 +0200

    update berzelius settings

--- berzelius.sh
+++ berzelius.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --nodes 1
 #SBATCH --gpus 1
-#SBATCH --time 3-00:00:00
+#SBATCH --time 1-00:00:00
 #SBATCH --output /proj/nlp4adas/users/%u/logs/%j.out
 #SBATCH -A berzelius-2022-232
 #
@@ -13,8 +13,8 @@ singularity exec --nv \
   --bind /proj/nlp4adas/users/$USER:/workspace \
   --bind /proj/nlp4adas/checkpoints:/checkpoints \
   --bind /proj/nlp4adas/features:/features \
-  --bind /proj/nlp4adas/datasets/once:/once \
-  --bind /proj/berzelius-2021-92/data/nuscenes:/nuscenes \
+  --bind /proj/adas-data/data/once:/once \
+  --bind /proj/adas-data/data/nuscenes:/nuscenes \
   --bind /proj/nlp4adas/users/$USER/lidar-clippin/SST/mmdet3d/ops/sst/sst_ops.py:/sst/mmdet3d/ops/sst/sst_ops.py \
   --bind /proj/nlp4adas/users/$USER/lidar-clippin/SST/mmdet3d/models/backbones/sst_v1.py:/sst/mmdet3d/models/backbones/sst_v1.py \
   --bind /proj/nlp4adas/users/$USER/lidar-clippin/SST/mmdet3d/models/backbones/sst_v2.py:/sst/mmdet3d/models/backbones/sst_v2.py \
